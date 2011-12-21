6c48ab5a3a17a6b359b0e0d827840074fc14fc9c
Nate Foster <jnfoster@cs.cornell.edu>
2011-12-20 23:13:58 -0800

    * Some tidying:   - moved nxtopo.py up to the top-level updates directory   - removed routing-nox.py
    * Extended routing example with a third update: bringing additional hosts online.
      This should be a case where the optimizationed per-packet implementation is a huge win
    
    * Added comments to routing.py

---
+++ updates/nxtopo.py
@@ -0,0 +1,66 @@
+from mininet.topo import Topo, Node
+import networkx as nx
+
+class NXTopo(nx.Graph):
+
+    def __init__(self):
+        super(NXTopo, self).__init__()
+        self.finalized=False
+
+    def add_switch(self,sid):
+        assert not self.finalized
+        self.add_node(sid, isSwitch=True)
+
+    def add_host(self,hid):
+        assert not self.finalized
+        self.add_node(hid, isSwitch=False)
+
+    def add_link(self,hid,sid):
+        assert not self.finalized
+        self.add_edge(hid,sid)
+        
+    def switches(self):
+        assert self.finalized
+        return [s for (s,d) in self.nodes(data=True) if d['isSwitch']]
+
+    def hosts(self):
+        assert self.finalized
+        return [h for (h,d) in self.nodes(data=True) if not d['isSwitch']]
+
+    def edge_switches(self):
+        result = set()
+        for h in self.hosts():
+            for s in self.neighbors(h):
+                if self.node[s]['isSwitch']:
+                    result.add(s)
+        return list(result)
+
+    def finalize(self):
+        # make mininet topo
+        topo = Topo()
+        
+        # add nodes
+        for x,d in self.nodes(data=True):
+            topo.add_node(x,Node(is_switch=d['isSwitch']))
+                
+        # add links
+        for src,dst in self.edges():
+            topo.add_edge(src,dst)
+            
+        # backpatch ports into original graph
+        for x in self.nodes():
+            self.node[x]['ports'] = {}
+            for y in self.neighbors(x):               
+                self.node[x]['ports'][y] = topo.port(x,y)[0]
+
+        topo.enable_all()
+        self.topo = topo        
+        self.finalized = True
+
+    def nx_graph(self):
+        assert self.finalized
+        return self.copy()
+
+    def mininet_topo(self):
+        assert self.finalized
+        return self.topo
